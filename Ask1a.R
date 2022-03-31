ae56bb2ef5a0e1bebf4093c4578b8d69525d9408
ntsipros <102686341+ntsipros@users.noreply.github.com>
2022-03-31 13:05:35 +0300

    Delete Ask1a.R

--- Ask1a.R
+++
@@ -1,7 +0,0 @@
-MC <- function(SAMPLE,a){
-  n <- length(SAMPLE)
-  i <- 1:n
-  result <- sum((SAMPLE[i] + a)^2)
-  print(result/n)
-}
-res <- data.frame(n = c(100,1000), a0 = c(MC(rnorm(100), 0),MC(rnorm(1000), 0)), a1 = c(MC(rnorm(100), 1),MC(rnorm(1000), 1)), a2 = c(MC(rnorm(100), 2),MC(rnorm(1000), 2)), a3 = c(MC(rnorm(100), 3),MC(rnorm(1000), 3)), a4 = c(MC(rnorm(100), 4),MC(rnorm(1000), 4)))
